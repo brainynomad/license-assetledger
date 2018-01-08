ab67ac748d7876b54fc36a639870ec82d586a19b
Rajadurai Balasubramanian <rbalasub@robomatter.com>
2018-01-07 19:41:59 -0500

    Revert "Revert "Initial Study about Ethereum dev tools and contract deployment.""
    This reverts commit 38704c7432395a3c1c9105ebea5c5a9cb6a47d7e.

---
+++ Ethereum/study/TestLicense.sol
@@ -0,0 +1,26 @@
+/*
+Created a first sample contract to understand the Dev toolchain and deployment process
+*/
+
+pragma solidity ^0.4.0;
+contract TestLicense {
+//product seller    
+address producer;
+address buyer;
+event purchase(address producer, address buyer );
+ function TestLicense(address _buyer) public {
+        producer = msg.sender;
+        buyer = _buyer;
+        purchase(producer,buyer);
+    }
+    
+function CheckLicense(address _buyer) public view returns (bool isEnabled) {
+        if (buyer == _buyer) 
+            return true;
+        return false;
+    }
+function GetmyAddress() public  constant  returns(address sender) {
+    sender = msg.sender;
+    return sender;
+    }
+}
